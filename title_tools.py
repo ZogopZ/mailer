637cd245a497916c526ea7895c3320aa442685ba
Zois Zogopoulos <z.zogopoulos@neurosoft.gr>
2020-07-27 15:46:33 +0300

    mailer-development_v19 -All modifications below took place in parent directory.
    -Resized army insignia to be sent in file 'title_tools.py'.

--- title_tools.py
+++ title_tools.py
@@ -82,6 +82,6 @@ class Title:
         with open(img_path, "rb") as img_file:
             enc_image = '<img src=\"data:image/png;base64,' +\
                         base64.b64encode(img_file.read()).decode('utf-8') +\
-                        '" width="50" height="70" alt="base64 test">'
+                        '" width="50" height="75" alt="base64 test">'
         title_list.extend([title, img_path, enc_image])
         return title_list
